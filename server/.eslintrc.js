7c395935f124c41b27f9ff2270b62333cb053e13
saifdev1642 <saifdev1642@gmail.com>
2020-08-05 03:57:48 +0600

    server setup

---
+++ server/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2020": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 11
+    },
+    "rules": {
+        "no-unused-vars":0
+    },
+    "globals": {
+        "process": true
+    }
+};
