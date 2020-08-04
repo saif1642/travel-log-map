3af6f9f6081763b503afc0c924ed207f0007d569
saifdev1642 <saifdev1642@gmail.com>
2020-08-05 04:12:19 +0600

    add middleware

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,9 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 
+
+const middleweres = require('./middleware');
+
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
@@ -16,20 +19,8 @@ app.get('/', (req, res) => {
     })
 })
 
-app.use((req, res, next) => {
-    const error = new Error(`Not Found - ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-})
-
-app.use((error, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    res.status(statusCode);
-    res.json({
-        message:error.message,
-        stack: process.env.NODE_ENV === 'production' ? ':(' : error.stack
-    })
-})
+app.use(middleweres.notFound);
+app.use(middleweres.errorHandaler);
 
 const port = process.env.PORT || 1338;
 
