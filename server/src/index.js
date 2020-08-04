a8b63d6e584cb4cc5e3259b5f29af7a2592bbe45
saifdev1642 <saifdev1642@gmail.com>
2020-08-05 04:49:50 +0600

    model added

--- server/src/index.js
+++ server/src/index.js
@@ -2,15 +2,20 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const middleweres = require('./middleware');
 
 const app = express();
+
+
+mongoose.createConnection(process.env.DATABASE_URL,{ useNewUrlParser:true });
+
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
-    origin:' http://localhost:3000'
+    origin:process.env.CORS_ORIGIN
 }));
 
 app.get('/', (req, res) => {
@@ -25,5 +30,5 @@ app.use(middleweres.errorHandaler);
 const port = process.env.PORT || 1338;
 
 app.listen(port, () => {
-    console.log(`app listen at http://localhost:${port}`)
+    console.log(`app listen at http://localhost:${port}`);
 })
